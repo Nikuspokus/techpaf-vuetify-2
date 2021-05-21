fcb57913bb8fdee676ccf09c0181fcfe92598473
Nicolas MARQUILLIER <nicolas.marquillier@sii.fr>
2021-05-21 17:04:50 +0200

    Feature Project - partie 2 en cours - RAF - boucler sur le composant - 3 - WIP

--- src/components/StoriesSection.vue
+++ src/components/StoriesSection.vue
@@ -43,8 +43,64 @@
           </v-layout>
         </div>
         <v-divider vertical color="0197bf" class="mx-10"></v-divider>
+        <div class="div-categoryContainer">
+          <!-- <v-card
+            elevation="12"
+            height="100"
+            width="170"
+            color="#1256FF"
+            class="mr-4"
+          >
+            <v-img
+              src="../assets/images/gptw.jpg"
+              contain
+              style="align:left"
+              class="picture1"
+            ></v-img>
 
-        <v-pagination circle v-model="page" :length="6"></v-pagination>
+            <h2><strong>Best Workplace 2021</strong></h2>
+            <v-spacer></v-spacer>
+            <p>
+              Il fait bon travailler chez SII et nous sommes cette année la
+              meilleure société labellisée parmi les plus grandes entreprises
+              auditées. Le bien-être et l’épanouissement au travail sont au cœur
+              de notre stratégie d’entreprise et de nos préoccupations. s
+            </p>
+          </v-card> -->
+          <v-container class="grey lighten-5">
+            <v-row no-gutters>
+              <v-col cols="6" md="4">
+                <v-card
+                  elevation="12"
+                  height="100"
+                  width="170"
+                  color="#1256FF"
+                  class="mr-4"
+                >
+                  <v-img
+                    src="../assets/images/gptw.jpg"
+                    contain
+                    style="align:left"
+                    class="picture1"
+                  ></v-img>
+                </v-card>
+              </v-col>
+              <v-col cols="12" sm="6" md="8">
+                
+                  <h2><strong>Best Workplace 2021</strong></h2>
+                  <v-spacer></v-spacer>
+                  <p>
+                    Il fait bon travailler chez SII et nous sommes cette année
+                    la meilleure société labellisée parmi les plus grandes
+                    entreprises auditées. Le bien-être et l’épanouissement au
+                    travail sont au cœur de notre stratégie d’entreprise et de
+                    nos préoccupations. s
+                  </p>
+              
+              </v-col>
+            </v-row>
+          </v-container>
+        </div>
       </v-layout>
 
       <v-spacer></v-spacer>
@@ -62,5 +118,33 @@ export default {};
 }
 .returnLine {
   white-space: pre;
+}
+body {
+  background-color: #ddd;
+}
+.div-categoryContainer {
+  box-sizing: border-box;
+  margin: 10px auto 10px;
+  padding: 10px;
+  width: 80%;
+  min-height: 300px;
+
+  font-family: sans-serif;
+  font-size: 1em;
+  color: #333;
+
+  background-color: #eee;
+}
+
+.picture2 {
+  border: none;
+  float: right;
+  margin-right: 40px;
+}
+
+.clearfix {
+  margin: 0, padding 0;
+  width: 0;
+  height: 0;
 }
 </style>
